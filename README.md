66f02c5ef5633babd048e6fa69bcd99a8d335ec7
andygulin <andygulin@hotmail.com>
2016-07-27 16:33:52 +0800

    ...

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# etcd-sample
+# etcd
+
+### 安装
+#### 安装golang，[golang下载](http://pan.baidu.com/s/1qYgq7Qc)
+	cd /data
+	tar zxvf go1.6.2.linux-amd64.tar.gz && mv go golang
+	vi /etc/profile
+	export GOROOT=/data/golang
+	export PATH=$GOROOT/bin:$PATH
+	source /etc/profile
+	
+#### 安装etcd
+	cd /data
+	curl -L https://github.com/coreos/etcd/releases/download/v3.0.3/etcd-v3.0.3-linux-amd64.tar.gz -o etcd-v3.0.3-linux-amd64.tar.gz
+	tar xzvf etcd-v3.0.3-linux-amd64.tar.gz && mv etcd-v3.0.3-linux-amd64 etcd && cd etcd
+	./etcd --version
+	
+	vi /etc/profile
+	export ETCD_NAME=centos
+	export ETCD_DATA_DIR=/data/etcd/data
+	export ETCD_LISTEN_PEER_URLS=http://0.0.0.0:2380
+	export ETCD_LISTEN_CLIENT_URLS=http://0.0.0.0:2379
+	export ETCD_ADVERTISE_CLIENT_URLS=http://0.0.0.0:2379
+	source /etc/profile
+	
+	./etcd &
--- src/main/java/etcd/sample/EtcdTest.java
+++ src/main/java/etcd/sample/EtcdTest.java
@@ -2,23 +2,20 @@ package etcd.sample;
 
 import java.io.IOException;
 import java.net.URI;
-import java.util.Map;
-import java.util.Map.Entry;
 import java.util.concurrent.TimeoutException;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import mousio.client.promises.ResponsePromise;
+import mousio.client.promises.ResponsePromise.IsSimplePromiseResponseHandler;
 import mousio.client.retry.RetryWithTimeout;
 import mousio.etcd4j.EtcdClient;
+import mousio.etcd4j.promises.EtcdResponsePromise;
 import mousio.etcd4j.responses.EtcdAuthenticationException;
 import mousio.etcd4j.responses.EtcdException;
 import mousio.etcd4j.responses.EtcdKeysResponse;
-import mousio.etcd4j.responses.EtcdLeaderStatsResponse;
-import mousio.etcd4j.responses.EtcdLeaderStatsResponse.FollowerInfo;
-import mousio.etcd4j.responses.EtcdSelfStatsResponse;
-import mousio.etcd4j.responses.EtcdStoreStatsResponse;
 import mousio.etcd4j.responses.EtcdVersionResponse;
 
 public class EtcdTest {
@@ -27,7 +24,7 @@ public class EtcdTest {
 
 	@Before
 	public void init() {
-		final String server = "http://192.168.137.130:2379";
+		final String server = "http://192.168.209.128:2379";
 		client = new EtcdClient(URI.create(server));
 		client.setRetryHandler(new RetryWithTimeout(200, 20000));
 	}
@@ -57,53 +54,20 @@ public class EtcdTest {
 	}
 
 	@Test
-	public void stat() {
-		EtcdLeaderStatsResponse leaderStatsResponse = client.getLeaderStats();
-		System.out.println(leaderStatsResponse.getLeader());
-		System.out.println("============================================");
-
-		Map<String, FollowerInfo> followerInfoMap = leaderStatsResponse.getFollowers();
-		for (Entry<String, FollowerInfo> entry : followerInfoMap.entrySet()) {
-			System.out.println(entry.getKey());
-			FollowerInfo followerInfo = entry.getValue();
-
-			System.out.println(followerInfo.getCounts().getSuccess());
-			System.out.println(followerInfo.getCounts().getFail());
-
-			System.out.println(followerInfo.getLatency().getAverage());
-			System.out.println(followerInfo.getLatency().getCurrent());
-			System.out.println(followerInfo.getLatency().getMaximum());
-			System.out.println(followerInfo.getLatency().getMinimum());
-			System.out.println(followerInfo.getLatency().getStandardDeviation());
-		}
-
-		System.out.println("============================================");
-
-		EtcdSelfStatsResponse selfStatsResponse = client.getSelfStats();
-		System.out.println(selfStatsResponse.getId());
-		System.out.println(selfStatsResponse.getName());
-		System.out.println(selfStatsResponse.getRecvAppendRequestCnt());
-		System.out.println(selfStatsResponse.getRecvBandwidthRate());
-		System.out.println(selfStatsResponse.getRecvPkgRate());
-		System.out.println(selfStatsResponse.getSendAppendRequestCnt());
-		System.out.println(selfStatsResponse.getState());
-		System.out.println(selfStatsResponse.getLeaderInfo().getLeader());
-		System.out.println(selfStatsResponse.getLeaderInfo().getUptime());
-		System.out.println(selfStatsResponse.getLeaderInfo().getStartTime());
-		System.out.println(selfStatsResponse.getStartTime());
-
-		System.out.println("============================================");
-
-		EtcdStoreStatsResponse storeStatsResponse = client.getStoreStats();
-		System.out.println(storeStatsResponse.getCompareAndSwapFail());
-		System.out.println(storeStatsResponse.getCompareAndSwapSuccess());
-		System.out.println(storeStatsResponse.getCreateFail());
-		System.out.println(storeStatsResponse.getCreateSuccess());
-		System.out.println(storeStatsResponse.getDeleteFail());
-		System.out.println(storeStatsResponse.getDeleteSuccess());
-		System.out.println(storeStatsResponse.getExpireCount());
-		System.out.println(storeStatsResponse.getGetsFail());
-		System.out.println(storeStatsResponse.getsSuccess());
+	public void promise() throws IOException, EtcdException, EtcdAuthenticationException, TimeoutException {
+		EtcdResponsePromise<EtcdKeysResponse> promise = client.put("key", "helloworld").send();
+		// on change
+		promise.addListener(new IsSimplePromiseResponseHandler<EtcdKeysResponse>() {
+			@Override
+			public void onResponse(ResponsePromise<EtcdKeysResponse> response) {
+				try {
+					printLog(response.get());
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+		System.in.read();
 	}
 
 	@After
