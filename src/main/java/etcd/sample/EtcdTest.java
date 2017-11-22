7065251d9eb402dfdb791f5b5bc58dce530c6c6e
andygulin <andygulin@hotmail.com>
2017-11-22 11:58:38 +0800

    版本更新

--- src/main/java/etcd/sample/EtcdTest.java
+++ src/main/java/etcd/sample/EtcdTest.java
@@ -10,78 +10,82 @@ import mousio.etcd4j.responses.EtcdVersionResponse;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.net.URI;
+import java.util.UUID;
 import java.util.concurrent.TimeoutException;
 
 public class EtcdTest {
 
-	private EtcdClient client;
+    private static final Logger LOGGER = LoggerFactory.getLogger(EtcdTest.class);
 
-	@Before
-	public void init() {
-		final String server = "http://192.168.209.128:2379";
-		client = new EtcdClient(URI.create(server));
-		client.setRetryHandler(new RetryWithTimeout(200, 20000));
-	}
+    private EtcdClient client;
 
-	@Test
-	public void version() {
-		EtcdVersionResponse response = client.version();
-		printLog(response);
-	}
+    @Before
+    public void init() {
+        final String server = "http://192.168.209.128:2379";
+        client = new EtcdClient(URI.create(server));
+        client.setRetryHandler(new RetryWithTimeout(200, 20000));
+    }
 
-	@Test
-	public void put() throws IOException, EtcdException, EtcdAuthenticationException, TimeoutException {
-		EtcdKeysResponse response = client.put("name", "aaa").send().get();
-		printLog(response);
-	}
+    @Test
+    public void version() {
+        EtcdVersionResponse response = client.version();
+        printLog(response);
+    }
 
-	@Test
-	public void get() throws IOException, EtcdException, EtcdAuthenticationException, TimeoutException {
-		EtcdKeysResponse response = client.get("name").send().get();
-		printLog(response);
-	}
+    @Test
+    public void put() throws IOException, EtcdException, EtcdAuthenticationException, TimeoutException {
+        for (int i = 1; i <= 100; i++) {
+            EtcdKeysResponse response = client.put("name" + i, UUID.randomUUID().toString()).send().get();
+            printLog(response);
+        }
+    }
 
-	@Test
-	public void delete() throws IOException, EtcdException, EtcdAuthenticationException, TimeoutException {
-		EtcdKeysResponse response = client.delete("name").send().get();
-		printLog(response);
-	}
+    @Test
+    public void get() throws IOException, EtcdException, EtcdAuthenticationException, TimeoutException {
+        EtcdKeysResponse response = client.get("name").send().get();
+        printLog(response);
+    }
 
-	@Test
-	public void promise() throws IOException, EtcdException, EtcdAuthenticationException, TimeoutException {
-		EtcdResponsePromise<EtcdKeysResponse> promise = client.put("key", "helloworld").send();
-		// on change
-		promise.addListener((response) -> {
-			try {
-				printLog(response.get());
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		});
-		System.in.read();
-	}
+    @Test
+    public void delete() throws IOException, EtcdException, EtcdAuthenticationException, TimeoutException {
+        EtcdKeysResponse response = client.delete("name").send().get();
+        printLog(response);
+    }
 
-	@After
-	public void close() {
-		try {
-			client.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+    @Test
+    public void promise() throws IOException, EtcdException, EtcdAuthenticationException, TimeoutException {
+        EtcdResponsePromise<EtcdKeysResponse> promise = client.put("key", "helloworld").send();
+        // on change
+        promise.addListener((response) -> {
+            try {
+                printLog(response.get());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+        System.in.read();
+    }
 
-	private void printLog(EtcdKeysResponse response) {
-		System.out.println(response.action);
-		System.out.println(response.node.key);
-		System.out.println(response.node.value);
-		System.out.println(response.node.ttl);
-	}
+    @After
+    public void close() {
+        try {
+            client.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-	private void printLog(EtcdVersionResponse response) {
-		System.out.println("server : " + response.server);
-		System.out.println("cluster : " + response.cluster);
-	}
+    private void printLog(EtcdKeysResponse response) {
+        LOGGER.info("Response : {} ", response.toString());
+    }
+
+    private void printLog(EtcdVersionResponse response) {
+        LOGGER.info("Server : {}", response.server);
+        LOGGER.info("Cluster : {}", response.cluster);
+    }
 }
